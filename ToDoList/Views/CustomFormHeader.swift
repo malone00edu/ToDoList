3c99db2ec795d4265e0e955538ddc8a8766d3cd4
malonethedev <malonethedev@gmail.com>
2024-05-01 09:41:48 -0400

    Changed the look of the Create a task form in NewItemView.

---
+++ ToDoList/Views/CustomFormHeader.swift
@@ -0,0 +1,18 @@
+//
+//  CustomFormHeader.swift
+//  ToDoList
+//
+//  Created by Taze Balbosa on 5/1/24.
+//
+
+import SwiftUI
+
+struct CustomFormHeader: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomFormHeader()
+}
